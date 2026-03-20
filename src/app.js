6d0d4fe5b840201eb4770c7b41640ad1d9ac7e82
obil david <elialexdaeli71@gmail.com>
2026-03-20 17:16:52 -0600

    fix cors definitivo

--- src/app.js
+++ src/app.js
@@ -49,6 +49,13 @@ app.use(cors({
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  next();
+});
+
 // 🔥 RUTAS
 app.use('/api/gallery', galleryRoutes);
 app.use('/api/settings', settingsRoutes);
